25dbabf52443bc31619c99586bec23a768ed76f8
Sven Marnach <sven@mozilla.com>
2026-04-20 10:49:16 +0200

    Add some documentation to the top level module.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! A library for uploading files to the Mozilla Symbols Server.
+//!
+//! This library provides a [`Client`] to upload a directory of files to the [Mozilla Symbols
+//! Server](https://symbols.mozilla.org/).
+
 use std::{
     path::{Path, PathBuf},
     sync::Arc,
@@ -5,6 +10,7 @@ use std::{
 use tokio::sync::Semaphore;
 use url::Url;
 
+/// Errors that may occur while uploading symbols.
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("not a directory: {0}")]
@@ -27,15 +33,29 @@ pub enum Error {
 
 type Result<T> = std::result::Result<T, Error>;
 
+/// The Mozill Symbols Server upload client.
+///
+/// The main functionality is provided by the [`Client::upload_directory`] method.
+///
+/// Clients are relatively cheap to clone. Clones will share the underlying [`reqwest::Client`]
+/// (which uses [`Arc`] internally) and the limit on concurrent connections to the server.
 #[derive(Clone, Debug)]
 pub struct Client {
     client: reqwest::Client,
     base_url: Url,
     auth_token: String,
+    /// The current upload API doesn't handle load spikes gracefully, so we limit the number
+    /// of concurrent connections.
     conn_limit_upload_v1: Arc<Semaphore>,
 }
 
 impl Client {
+    /// Create a new upload client.
+    ///
+    /// The [`reqwest::Client`] should have a meaningful, custom user agent. The `base_url` of
+    /// the production Mozilla Symbols Server is https://symbols.mozilla.org/. You can can
+    /// obtain an `auth_token` from the web interface of the symbols server (provided you have
+    /// an account with upload permissions).
     pub fn new<S: Into<String>>(client: reqwest::Client, base_url: Url, auth_token: S) -> Self {
         Self {
             client,
@@ -45,6 +65,9 @@ impl Client {
         }
     }
 
+    /// Upload a directory on the filesystem to the symbols server.
+    ///
+    /// The files to be uploaded are discovered using [`sym_files::discover`].
     pub async fn upload_directory<P: AsRef<Path>>(&self, path: P) -> Result<()> {
         let path = std::fs::canonicalize(path.as_ref())?;
         if !path.is_dir() {
@@ -53,6 +76,7 @@ impl Client {
         v1::upload_directory(self, &path).await
     }
 
+    /// Perform an authenticated request to the symbols server.
     fn request(&self, method: reqwest::Method, path: &str) -> Result<reqwest::RequestBuilder> {
         let builder = self
             .client
