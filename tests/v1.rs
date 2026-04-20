8b35596a6ab657b9c51c9d24f1da86fc0a4ff9b5
Sven Marnach <sven@mozilla.com>
2026-04-20 15:56:48 +0200

    Use the #[tokio::test] macro for the integration test.

--- tests/v1.rs
+++ tests/v1.rs
@@ -5,20 +5,16 @@
 
 #![cfg(feature = "local-upload-test")]
 
-use tokio::runtime::Runtime;
 use upload_symbols::Client;
 use url::Url;
 
-#[test]
-fn upload_directory_locally() {
+#[tokio::test]
+async fn upload_directory_locally() {
     let auth_token = std::env::var("LOCAL_AUTH_TOKEN").unwrap();
     let client = Client::builder(auth_token)
         .base_url(Url::parse("http://localhost:8000/").unwrap())
         .zip_size_threshold_v1(1 << 20) // 1 MiB
         .build()
         .unwrap();
-    Runtime::new()
-        .unwrap()
-        .block_on(client.upload_directory("tests/data/linux"))
-        .unwrap();
+    client.upload_directory("tests/data/linux").await.unwrap();
 }
