22d3a7fe4bd4c29e0c1dbcd0c1f525ea9e51d95d
Sven Marnach <sven@mozilla.com>
2026-05-20 20:11:06 +0200

    bug-2037395: Add docstrings and comments to the OTel code.

--- src/opentelemetry.rs
+++ src/opentelemetry.rs
@@ -19,6 +19,16 @@ type InternalLayer<S> = OpenTelemetryLayer<S, SdkTracer>;
 type Filter<S> = reload::Layer<LevelFilter, S>;
 pub type Layer<S> = Filtered<reload::Layer<Option<InternalLayer<S>>, S>, Filter<S>, S>;
 
+/// Coordinate setting up OpenTelemetry exporters.
+///
+/// The tracing library is set up at the start of the program, but we only retrieve the
+/// [`OpenTelemetryConfig`] during the preflight request to the Symbols Server. For this reason
+/// we need to add a placeholder layer to the tracing subscriber during start-up, which we can
+/// replace with the actual [`OpenTelemetryLayer`] once we have the configuration. We need to
+/// wrap both the OpenTelemetry layer and the [`LevelFilter`] for that layer in a
+/// [`reload::Layer`] to make them dynamically configurable.
+///
+/// The type parameter `S` represents that subscriber type.
 pub struct Coordinator<S> {
     layer_handle: reload::Handle<Option<InternalLayer<S>>, S>,
     filter_handle: reload::Handle<LevelFilter, S>,
@@ -30,8 +40,16 @@ where
     S: Subscriber,
     for<'a> S: LookupSpan<'a>,
 {
+    /// Create a new [`Coordinator`] instance.
+    ///
+    /// The constructor also returns the placeholder layer that should be added to the tracing
+    /// subscriber.
     pub fn new() -> (Self, Layer<S>) {
+        // Create the placeholder layer. The [`tracing_subscriber::layer::Layer`] trait is
+        // implemented for `Option<impl Layer>` as well, with `None` representing a no-op
+        // layer.
         let (layer, layer_handle) = reload::Layer::new(None::<InternalLayer<S>>);
+        // Create a filter that disables the layer (which doesn't do anything anyway).
         let (filter, filter_handle) = reload::Layer::new(LevelFilter::OFF);
         let manager = Self {
             layer_handle,
@@ -42,13 +60,23 @@ where
     }
 }
 
+/// Trait to erase the subscriber type parameter from [`Coordinator`].
+///
+/// We want to be able to return a [`Coordinator`] instance from functions, but it's often hard
+/// or impossible to name the subscriber type `S`. Using this trait, we can return `impl
+/// OTelCoordinator` instead, so we don't have to explicitly name the subscriber type, without
+/// the need for any dynamic dispatch.
 pub trait OTelCoordinator {
     fn set_up_otlp(&mut self, config: &OpenTelemetryConfig) -> Result<()>;
     fn shutdown(&self) -> Result<()>;
 }
 
-fn build_endpoint(base: &str, path: &str) -> String {
-    format!("{}{path}", base.trim_end_matches('/'))
+/// Add a path to the base endpoint URL.
+///
+/// We get a OpenTelemetry base URL from the Symbols Server. We still need to append paths like
+/// `v1/traces` or `v1/metrics` for the individual services.
+fn build_endpoint(base_url: &str, path: &str) -> String {
+    format!("{}{path}", base_url.trim_end_matches('/'))
 }
 
 impl<S> OTelCoordinator for Coordinator<S>
@@ -56,6 +84,10 @@ where
     S: Subscriber,
     for<'a> S: LookupSpan<'a>,
 {
+    /// Replace the placeholder layer and filter with the actual instances.
+    ///
+    /// This function sets up an [`OpenTelementryLayer`] and a [`LevelFilter`] based on the
+    /// given configuration and replaces the placeholders with them.
     fn set_up_otlp(&mut self, config: &OpenTelemetryConfig) -> Result<()> {
         let resource = Resource::builder()
             .with_service_name("upload-symbols")
@@ -85,6 +117,7 @@ where
         Ok(())
     }
 
+    /// Shut down all OTel providers to flush all remaining data to the collector.
     fn shutdown(&self) -> Result<()> {
         if let Some(ref provider) = self.tracer_provider {
             provider.shutdown()?;
